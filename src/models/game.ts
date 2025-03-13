96adc51f7cd9050e4492bbfdb2fabf4ec9c6bc1c
TimoRenken <timorenken@outlook.de>
2025-03-13 09:58:39 +0100

    added a function to shuffle the card stack

--- src/models/game.ts
+++ src/models/game.ts
@@ -10,6 +10,30 @@ export class Game {
             this.stack.push( i + '_of_diamonds.png')
             this.stack.push( i + '_of_hearts.png')
             this.stack.push( i + '_of_spades.png')
+            shuffle(this.stack);
         }
     }
 }
+
+// code from stackoverflow , just for testing
+
+function shuffle(array:any) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
+  
+  // Used like so
+  let arr = [2, 11, 37, 42];
+  shuffle(arr);
+  console.log(arr);
